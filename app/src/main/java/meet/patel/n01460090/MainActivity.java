e8566e53b2d5addeef9a4aac8dec400529195a4b
Meet2304 <meetpatel12302@gmail.com>
2022-07-25 15:03:14 -0400

    Initial Commit

---
+++ app/src/main/java/meet/patel/n01460090/MainActivity.java
@@ -0,0 +1,14 @@
+package meet.patel.n01460090;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
