5eeaa39e2b0e22e3c047a8c40c3dd9999a684f7b
Meet2304 <meetpatel12302@gmail.com>
2022-07-31 00:00:16 -0400

    Weather Fragment Completed

--- MeetAssign44/src/main/java/meet/patel/n01460090/N01460090Weather.java
+++ MeetAssign44/src/main/java/meet/patel/n01460090/N01460090Weather.java
@@ -7,58 +7,133 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link N01460090Weather#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class N01460090Weather extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Spinner spinner;
+    String Toronto = "Toronto", NewDelhi = "New Delhi", Sydney = "Sydney", London = "London", Paris = "Paris";
+    String Canada = "Canada", India = "India", Australia = "Australia", UK = "United Kingdom", France = "France";
+    TextView Country, Name, Latitude, Longitude, Humidity, Description;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private final String api = "9662ce8dd47b0fb078e29c318ebf7c31";
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
 
     public N01460090Weather() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment N01460090Weather.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static N01460090Weather newInstance(String param1, String param2) {
-        N01460090Weather fragment = new N01460090Weather();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.n01460090_weather, container, false);
+       View view = inflater.inflate(R.layout.n01460090_weather, container, false);
+
+        Country = (TextView) view.findViewById(R.id.MeetWeatherCountryTextView2);
+        Name = (TextView) view.findViewById(R.id.MeetWeatherCityTextView2);
+        Latitude = (TextView) view.findViewById(R.id.MeetWeatherLatitudeTextView2);
+        Longitude = (TextView) view.findViewById(R.id.MeetWeatherLongitudeTextView2);
+        Humidity = (TextView) view.findViewById(R.id.MeetWeatherHumidityTextView2);
+        Description = (TextView) view.findViewById(R.id.MeetWeatherDescriptionTextView2);
+
+        spinner = (Spinner) view.findViewById(R.id.MeetWeatherSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.cities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        getWeatherDetails(Toronto,Canada);
+                        break;
+                    case 1:
+                        getWeatherDetails(London,UK);
+                        break;
+                    case 2:
+                        getWeatherDetails(NewDelhi,India);
+                        break;
+                    case 3:
+                        getWeatherDetails(Sydney,Australia);
+                        break;
+                    case 4:
+                        getWeatherDetails(Paris,France);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+       return view;
+    }
+
+    private void getWeatherDetails(String city, String country){
+        String tempUrl = url + "?q=" + city + "," + country + "&appid=" + api;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String output = "";
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("weather");
+                    JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                    String description = jsonObjectWeather.getString("description");
+                    JSONObject jsonObjectMain = jsonObject.getJSONObject("main");
+                    int humidity = jsonObjectMain.getInt("humidity");
+                    JSONObject jsonObjectSys = jsonObject.getJSONObject("sys");
+                    String countryName = jsonObjectSys.getString("country");
+                    String cityName = jsonObject.getString("name");
+                    JSONObject jsonObjectCoord = jsonObject.getJSONObject("coord");
+                    double longitude = jsonObjectCoord.getDouble("lon");
+                    double latitude = jsonObjectCoord.getDouble("lat");
+                    String convLon = Double.toString(longitude);
+                    String convLat = Double.toString(latitude);
+                    String convHum = Integer.toString(humidity);
+
+                    Country.setText(countryName);
+                    Name.setText(cityName);
+                    Latitude.setText(convLat);
+                    Longitude.setText(convLon);
+                    Humidity.setText(convHum + "°C");
+                    Description.setText(description);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), getString(R.string.weather_toast_error), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
     }
 }
